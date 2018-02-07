a35ad9db201da79110a7b9dc2647da989bcb895d
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2018-02-07 14:19:30 -0500

    Exension successfully sends web requests to python script, which outputs them to a file.

--- extension_messaging/app/user_app.py
+++ extension_messaging/app/user_app.py
@@ -4,6 +4,11 @@ import sys
 import json
 import struct
 
+
+# Can change file name to specify date/time when run?
+outputFile = open("requests.txt", "w+")
+outputFile.write("Web requests being made on page: \n\n")
+
 try:
     # Python 3.x version
     # Read a message from stdin and decode it.
@@ -32,16 +37,26 @@ try:
         receivedMessage = getMessage()
         if receivedMessage == "ping":
             sendMessage(encodeMessage("pong3"))
+
 except AttributeError:
     # Python 2.x version (if sys.stdin.buffer is not defined)
     # Read a message from stdin and decode it.
     def getMessage():
         rawLength = sys.stdin.read(4)
+
         if len(rawLength) == 0:
             sys.exit(0)
+        
         messageLength = struct.unpack('@I', rawLength)[0]
+
         message = sys.stdin.read(messageLength)
-        return json.loads(message)
+
+        # Unpack message and write to output file
+        toWrite = json.loads(message)
+        outputFile.write(toWrite)
+        outputFile.write("\n")
+    
+        return toWrite
     
     # Encode a message for transmission,
     # given its content.
@@ -58,5 +73,10 @@ except AttributeError:
     
     while True:
         receivedMessage = getMessage()
-        if receivedMessage == "ping":
-            sendMessage(encodeMessage("pong2"))
+
+        if len(receivedMessage) > 0:
+            sendMessage(encodeMessage("PYTHON SCRIPT: Request info received,  written to output file."))
+        else:
+            sendMessage(encodeMessage("PYTHON SCRIPT: Error receiving message."))
+
+    outputFile.close()
--- extension_messaging/extension/background.js
+++ extension_messaging/extension/background.js
@@ -1,19 +1,40 @@
-/*
-On startup, connect to the "ping_pong" app.
-*/
+
+// On startup, connect to the "user_app" app.
 var port = browser.runtime.connectNative("user_app");
 
-/*
-Listen for messages from the app.
-*/
-port.onMessage.addListener((response) => {
-  console.log("Received: " + response);
+
+// Prints html request info to log
+function logURL(requestDetails) 
+{ 
+  console.log("EXTENSION: Sending webrequest info to python script.");
+  port.postMessage(requestDetails.url);
+}
+
+
+// Listen for messages from the app.
+port.onMessage.addListener((response) => 
+{
+  console.log(response);
 });
 
+
 /*
-On a click on the browser action, send the app a message.
-*/
-browser.browserAction.onClicked.addListener(() => {
-  console.log("Sending:  ping");
-  port.postMessage("ping");
+// On a click on the browser button, send the user_app a message.
+browser.browserAction.onClicked.addListener(() => 
+{
+  console.log("Sending request details to python script.");
+  port.postMessage(requestDetails.url);
+
+  //port.postMessage("ping");
 });
+*/
+
+// Listner for web requests, sends them to logURL
+browser.webRequest.onBeforeRequest.addListener
+(
+    logURL,
+    {urls: ["<all_urls>"]}
+);
+
+
+
